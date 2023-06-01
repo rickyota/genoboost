9dc93c892486922de5ec6c3294208126a790d648
rickyota <22293266+rickyota@users.noreply.github.com>
2023-06-01 16:06:01 +0900

    add: github action publish

---
+++ create.publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+d_publish="./publish_tmp/"
+
+bash ./build.static.sh
+mkdir -p ${d_publish}
+cp ./projects_rust/target/x86_64-unknown-linux-musl/release/genoboost ${d_publish}/genoboost
+
+mkdir -p ${d_publish}
+cp ./test/data/toy1/* ${d_publish}/sample/
+
+
+zip -r ./genoboost.zip ${d_publish}
+
