9ec36d2d151141e296f08f571c67caa2483da454
rickyota <22293266+rickyota@users.noreply.github.com>
2023-11-24 19:18:53 +0900

    fix: add [version] in Cargo.toml

--- genoboost.sh
+++ genoboost.sh
@@ -27,8 +27,7 @@ cp ./projects_rust/target/release/genoboost ./genoboost
     --file-genot "$file_plink" \
     --file-phe "$file_cov" \
     --cov age,sex \
-    --major-a2-train \
-    --verbose
+    --major-a2-train
 
 # score
 ./genoboost score \
